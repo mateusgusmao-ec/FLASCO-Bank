1c52a8a2e60287f34a31a8d52bcc38ba15279fb7
ArthurMaciel-ec <arthur.maciel@dcomp.ufs.br>
2026-08-28 16:41:28 -0300

    Implement client registration functions
    Added functions for client registration.

--- cliente.py
+++ cliente.py
@@ -1,1 +1,11 @@
+# Funções que diz respeito ao cadastro do cliente
 
+def cadastro_cliente(nome, cpf, senha):
+    nome = input('Insira seu nome: ')
+    cpf = int(input('Insira seu cpf: '))
+    senha = input('Insira sua senha (somente letras):')
+
+    return nome, cpf, senha
+
+def Cliente(cadastro_cliente):
+    cadastro_cliente(None, None, None)
