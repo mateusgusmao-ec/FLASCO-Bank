2cbc14c214ab6157e7a95a1b2e6d8fe7d61c6f9f
ArthurMaciel-ec <arthur.maciel@dcomp.ufs.br>
2026-08-28 16:46:20 -0300

    Update cadastro_cliente function with hardcoded values

--- cliente.py
+++ cliente.py
@@ -1,11 +1,8 @@
 # Funções que diz respeito ao cadastro do cliente
 
 def cadastro_cliente(nome, cpf, senha):
-    nome = input('Insira seu nome: ')
-    cpf = int(input('Insira seu cpf: '))
-    senha = input('Insira sua senha (somente letras):')
+    nome = 'João'
+    cpf = '98794416567'
+    senha = 'senhateste'
 
     return nome, cpf, senha
-
-def Cliente(cadastro_cliente):
-    cadastro_cliente(None, None, None)
