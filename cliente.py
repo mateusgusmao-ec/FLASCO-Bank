92fb3b8028289ba43f8905eba04a8b6cc1e2fc17
ArthurMaciel-ec <arthur.maciel@dcomp.ufs.br>
2026-08-30 15:04:46 -0300

    Arquivo cliente.py 01

--- cliente.py
+++ cliente.py
@@ -1,4 +1,6 @@
 # Funções que diz respeito ao cadastro do cliente
+# Login do cliente
+# BASEADO NAS FUNÇOES MAIN.PY
 
 def cadastro_cliente(nome, cpf, data_nascimento):
     cadastro = f'Cliente: {nome} | CPF: {cpf} | Data de Nascimento: {data_nascimento}'
