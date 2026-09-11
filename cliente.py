a5b790a2393cc11d46dc6a85c1a320d85672aad8
ArthurMaciel-ec <arthur.maciel@dcomp.ufs.br>
2026-09-11 16:16:27 -0300

    Adicionei criar_clienteS para a etapa 2

--- cliente.py
+++ cliente.py
@@ -5,3 +5,14 @@
 def criar_cliente(nome, cpf, data_nascimento):
     cadastro = f'Cliente: {nome} | CPF: {cpf} | Data de Nascimento: {data_nascimento}'
     return cadastro
+
+def criar_clienteS(nome, cpf, data_nascimento):
+    clientes = []
+    
+    quantidade_clientes = int(input('Quantos clientes serão cadastrados? '))
+
+    for _ in range(quantidade_clientes):
+        nome = input('Insira seu nome: ')
+        cpf = int(input('Insira seu cpf: '))
+        data_nascimento = int(input('Insira a sua data de nascimento: '))
+        clientes.append(nome)
