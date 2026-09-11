c9fd3f0342fb2ca19fec8377b3a4e1f624007344
ArthurMaciel-ec <arthur.maciel@dcomp.ufs.br>
2026-09-11 16:17:27 -0300

    Criei o menu_opcoes para a etapa 2

--- menu_opcoes.py
+++ menu_opcoes.py
@@ -1,1 +1,25 @@
+from cliente import criar_clienteS
+from conta import criar_contaS
+
+print('BEM VINDO AO FLASCO-BANK')
+print('1 - Cadastro cliente')
+print('2 - Criar conta')
+print('5 - Sair da conta')
+
+operacao = int(input('Insira o código de operação: '))
+
+while operacao != 5:
+        if operacao == 1:
+            criar_clienteS(nome, cpf, data_nascimento)
+
+        elif operacao == 2:
+            criar_contaS(saldoConta, )
+        
+        
+        elif operacao > 5:
+            print('Não existe esse codigo de operação, insira novamente')
+        
+        operacao = int(input('Insira o código de operação: '))
+                  
+
 
