5b2550ac2759fe52ea80a08cf960693d82095b51
ArthurMaciel-ec <arthur.maciel@dcomp.ufs.br>
2026-09-17 00:16:27 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -5,3 +5,17 @@
 def criar_cliente(nome, cpf, data_nascimento):
     cadastro = f'Cliente: {nome} | CPF: {cpf} | Data de Nascimento: {data_nascimento}'
     return cadastro
+
+def criar_clienteS(nome, cpf, gata_nascimento):
+    cliente = []
+
+    quantidade = int('Quantos clientes serão cadastrados? ')
+
+    if cpf not in clientes: 
+        for _ in range(quantidade):
+            nome = input()
+            cpf = int(input())
+            data_nascimento = int(input())
+            cliente.append(cpf)
+    else:
+        print('Este cliente já existe.')
