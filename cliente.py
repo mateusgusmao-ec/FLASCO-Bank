80bc94b146a6ed35e8bc05c82db71caf128991f0
vinlevi <levi.oliveira@dcomp.ufs.br>
2026-09-24 15:44:20 -0300

    adicionando comentarios detalhadando as funçoes
    Added comments to clarify the purpose of functions and logic in the client registration process.

--- cliente.py
+++ cliente.py
@@ -2,23 +2,24 @@
 # Login do cliente
 # BASEADO NAS FUNÇOES MAIN.PY
 ListaClientes = []
+#FUNÇAO RESPONSAVEL PELA EXIBIÇAO DOS DADOS DO CLIENTE
 def exibirCliente(nome, cpf, data_nascimento):
     cadastro = f'Cliente: {nome} | CPF: {cpf} | Data de Nascimento: {data_nascimento}'
     return cadastro
 
 def cadastrarClientes(ListaClientes):
     quantidade = int(input('Quantos clientes serão cadastrados? '))
-    for item in range(quantidade):
-        cpf = str(input('digite seu cpf para cadastro'))
+    for item in range(quantidade): #cria um laço com range da quantidade de clientes que serao cadastrados
+        cpf = str(input('digite seu cpf para cadastro')) 
         cpfExiste = False
         for j in range(len(ListaClientes)):
-            if ListaClientes[j][0] == cpf:
+            if ListaClientes[j][0] == cpf: #faz uma busca na lista dos clientes por indice e puxa a o primeiro indice da tupla.
                 cpfExiste = True
                 break
         if cpfExiste:
              print('cliente ja cadastrado')
         else:
-            nome = str(input('digite seu nome'))
+            nome = str(input('digite seu nome')) #caso o cpf nao esteja nas tuplas que estao dentro da lista dos clientes, o cliente ainda nao foi cadastrado
             data_nascimento = str(input('digite sua data de nascimento'))
             cliente = (cpf,nome,data_nascimento)
             ListaClientes.append(cliente)
@@ -30,7 +31,7 @@ def listar_clientes(ListaClientes):
         return
     print('LISTA DE CLIENTES')
     for item in range(len(ListaClientes)):
-        cliente = ListaClientes[item]
+        cliente = ListaClientes[item] #lista os clientes baseado nos dados
         print(f"Cliente {item + 1}:")
         print(f"  CPF                : {cliente[0]}")
         print(f"  Nome               : {cliente[1]}")
