4340abb8f02e9f7f753087be190707ea0ad28a9f
mateusgusmao-ec <mateusgusmao@dcomp.ufs.br>
2026-09-24 22:07:10 -0300

    ordenação, correção no cadastro de clientes

--- cliente.py
+++ cliente.py
@@ -3,30 +3,40 @@
 # BASEADO NAS FUNÇOES MAIN.PY
 
 def cadastrar_cliente(nome, cpf, data_nascimento):
-    cadastro = f'Cliente: {nome} | CPF: {cpf} | Data de Nascimento: {data_nascimento}'
-    return cadastro
+    return f'Cliente: {nome} | CPF: {cpf} | Data de Nascimento: {data_nascimento}'#retorna os dados numa tupla
 
 def cadastrar_clienteS(nome, cpf, data_nascimento):
     clientes = []
 
     quantidade = int(input('Quantos clientes serão cadastrados? '))
-    
-    if cpf not in clientes: 
-        for _ in range(quantidade):
-            nome = input()
-            cpf = int(input())
-            data_nascimento = int(input())
-            clientes.append(nome, cpf)
-    else:
-        print('Este cliente já existe.')
+    for i in range(quantidade):
+        nome = input()
+        cpf = int(input())
+        data_nascimento = int(input())
 
-def ordenar_clientes()  # Usar .sort() 
-    
+        cpf_existe = False
+        for cliente in clientes :
+            if cliente[1] == cpf:
+                cpf_existe = True
+                break #usando break para interromper o cadastro se o cpf ja existir
+        if not cpf_existe :
+            #criar tupla e adicionar o novo cliente
+            novo_cliente = cadastrar_cliente(nome, cpf, data_nascimento)
+            clientes.append(novo_cliente)
+            print("Cliente cadastrado com sucesso!")
+        else:
+            print(f"O CPF {cpf} já existe. Cliente não encontrado")
+def obter_nome_Cliente(cliente):
+    return cliente[0]#retorna o nome puro
+
+def ordenar_clientes(clientes)  # Usar .sort() 
+    #ordenação em ordem alfabética com base no nome (lista[0])
+    clientes.sort(key= obter_nome_Cliente)
+    print("Clientes ordenados!")
     
 
 
 def listar_clientes(clientes):
     print("\n--- CLIENTES ---")
-
-    for cpf in clientes:
-        print("CPF:", cpf)
+    for cliente in clientes:
+        print(f"Nome : {cliente[0]} | CPF : {cliente[1]} | Nascimento : {cliente[2]}")
